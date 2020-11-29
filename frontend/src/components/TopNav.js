b0a1c348f98bff50280890828a0c55db7e8018ad
2394958r <2394958r@student.gla.ac.uk>
2020-11-29 16:27:48 +0000

    remodeled homepage and navigation sidebar

--- frontend/src/components/TopNav.js
+++ frontend/src/components/TopNav.js
@@ -1,69 +1,105 @@
 import React, { useState, useContext } from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, NavbarText, UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap';
-import { Link } from 'react-router-dom';
+//import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, NavbarText, UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap';
+import { Link, useHistory } from 'react-router-dom';
 import { UserContext } from '../user-context'
 import './TopNav.css'
 import logo from '../assets/logo.jpg'
+import api from '../services/api'
 
-const TopNav = () => {
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+
+export default function TopNav() {
+    let history = useHistory()
     const { isLoggedIn, setIsloggedIn } = useContext(UserContext);
     const userName = localStorage.getItem('userName')
-    const [isOpen, setIsOpen] = useState(false);
+    
 
-    const toggle = () => setIsOpen(!isOpen);
 
-    const [collapsed, setCollapsed] = useState(true);
-    const toggleNavbar = () => setCollapsed(!collapsed);
+    return (
+        <SideNav id="SideNav" onSelect={(selected) => {
+            if(typeof(selected) !== "undefined"){
+                if(selected === "logout"){
+                    localStorage.removeItem('user')
+                    localStorage.removeItem('user_id')
+                    localStorage.removeItem('profilePic')
+                    localStorage.removeItem('userName')
+                    setIsloggedIn(false)
+                    history.push("/login")
+                }
+                if(selected === "profile"){
+                    history.push("/profile") 
+                }
+                if(selected === "home"){
+                    history.push("/") 
+                }
+                
+            }
+        }}>
+        <SideNav.Toggle id="SideNavToggle" />
+            <SideNav.Nav defaultSelected="home">
 
-    const logoutHandler = () => {
-        localStorage.removeItem('user')
-        localStorage.removeItem('user_id')
-        setIsloggedIn(false)
-    }
-    console.log(window.location.origin)
-    return isLoggedIn ? 
-        <div>
-{/*             <Navbar color="faded" light>
-                <Nav navbar>
-                    <NavItem>
-                        <Link to="/user">Profile</Link>
+                <NavItem id="NavItemLogo">
+                    <NavIcon>
+                        <img className="logo" src={logo}/>
+                    </NavIcon>
+                    <NavText id="NavTextLogo">
+                        CISmap
+                    </NavText>
+                </NavItem>
+                
+                { isLoggedIn ? (
+                <NavItem eventKey="profile_dropdown" id="NavItem">
+                       <NavIcon>
+                         <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+                    </NavIcon>
+                     <NavText id="NavText">
+                          {localStorage.userName}
+                      </NavText>
+                      <NavItem eventKey="profile" id="NavItem">
+                         <NavText id="NavTextSub">
+                             Profile
+                        </NavText>
                     </NavItem>
-                    <NavItem>
-                        <Link to="/events">Events</Link>
+                     <NavItem eventKey="logout" id="NavItem">
+                         <NavText id="NavTextSub">
+                              Logout
+                          </NavText>
                     </NavItem>
-                    <NavItem>
-                        <Link to="/">Dashboard</Link>
-                    </NavItem>
-                </Nav>
-                <Link to="/login" onClick={logoutHandler}>Logout</Link>
-            </Navbar> */}
-            
+                </NavItem>
+                ) : ""
+                }
 
-            <Navbar color="light" light expand="md">
-                <img className="logo" src={logo}/>
-                <NavbarBrand href="/"> CISmap (Working title) </NavbarBrand>
-                <NavbarToggler onClick={toggle} />
-                <Collapse isOpen={isOpen} navbar>
-                    <Nav navbar className="container-fluid">
-                        <UncontrolledDropdown nav inNavbar className="ml-auto">
-                            <DropdownToggle nav caret>
-                                {userName}
-                            </DropdownToggle>
-                            <DropdownMenu right>
-                                <DropdownItem>
-                                    <NavLink href="/profile">Profile</NavLink>
-                                </DropdownItem>
-                                <DropdownItem divider />
-                                <DropdownItem>
-                                    <NavLink href="/login" onClick={logoutHandler}>Logout</NavLink>
-                                </DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                    </Nav>
-                </Collapse>
-            </Navbar>
-        </div>
-    : ""
+                <NavItem eventKey="home" id="NavItem">
+                    <NavIcon>
+                        <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                    </NavIcon>
+                    <NavText id="NavText">
+                        Home
+                    </NavText>
+                </NavItem>
+
+                <NavItem eventKey="maps" id="NavItem">
+                    <NavIcon>
+                        <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+                    </NavIcon>
+                    <NavText id="NavText">
+                        Maps
+                    </NavText>
+                    <NavItem eventKey="maps/browse" id="NavItem">
+                        <NavText id="NavTextSub">
+                            Browse Maps
+                        </NavText>
+                    </NavItem>
+                    <NavItem eventKey="maps/mymaps" id="NavItem">
+                        <NavText id="NavTextSub">
+                            My Maps
+                        </NavText>
+                    </NavItem>
+                </NavItem>
+            </SideNav.Nav>
+        </SideNav>
+    )
 }
-
-export default TopNav;
--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,11 +1,9 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import api from '../../services/api';
 import moment from 'moment';
-import { Button, ButtonGroup, Container, Alert, Dropdown, Nav, NavLink, 
-    DropdownItem, DropdownMenu, DropdownToggle, ListGroup, ListGroupItem, Card, CardBody, CardImg, CardTitle, CardSubtitle, CardText } from 'reactstrap';
+import { Button, ButtonGroup, Alert, Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap';
 import socketio from 'socket.io-client';
 import './dashboard.css'
-import battle from '../../assets/battle.jpg'
 
 
 export default function Dashboard({ history }) {
@@ -17,8 +15,12 @@ export default function Dashboard({ history }) {
     const [error, setError] = useState(false);
     const [success, setSuccess] = useState(false)
     const [messageHandler, setMessageHandler] = useState('')
-    const [browseMapsPanel, setBrowseMapsPanel] = useState(false)
-    const [myMapsPanel, setMyMapsPanel] = useState(false)
+    const [eventsRequest, setEventsRequest] = useState([])
+    const [dropdownOpen, setDropDownOpen] = useState(false)
+    const [eventRequestMessage, setEventRequestMessage] = useState('')
+    const [eventRequestSuccess, setEventRequestSuccess] = useState(false)
+
+    const toggle = () => setDropDownOpen(!dropdownOpen);
 
     useEffect(() => {
         getEvents()
@@ -29,6 +31,10 @@ export default function Dashboard({ history }) {
             socketio('http://localhost:8000/', { query: { user: user_id } }),
         [user_id]
     );
+
+    useEffect(() => {
+        socket.on('registration_request', data => setEventsRequest([...eventsRequest, data]));
+    }, [eventsRequest, socket])
 
     const filterHandler = (query) => {
         setRSelected(query)
@@ -79,78 +85,121 @@ export default function Dashboard({ history }) {
         }
     }
 
-    console.log("middle panel: ", browseMapsPanel)
+    const registrationRequestHandler = async (event) => {
+        try {
+            await api.post(`/registration/${event.id}`, {}, { headers: { user } })
+            setSuccess(true)
+            setMessageHandler(`The request for the event ${event.title} was successfully!`)
+            setTimeout(() => {
+                setSuccess(false)
+                filterHandler(null)
+                setMessageHandler('')
+            }, 2500)
 
-    const browseMapsFunc = async() => {
-        setBrowseMapsPanel(!browseMapsPanel)
-        if (myMapsPanel){
-            setMyMapsPanel(false)
+        } catch (error) {
+            setError(true)
+            setMessageHandler(`The request for the event ${event.title} wasn't successfully!`)
+            setTimeout(() => {
+                setError(false)
+                setMessageHandler('')
+            }, 2000)
         }
     }
 
-    const myMapsFunc = async() => {
-        setMyMapsPanel(!myMapsPanel)
-        if (browseMapsPanel){
-            setBrowseMapsPanel(false)
+    const acceptEventHandler = async (eventId) => {
+        try {
+            await api.post(`/registration/${eventId}/approvals`, {}, { headers: { user } })
+            setEventRequestSuccess(true)
+            setEventRequestMessage('Event approved successfully!')
+            removeNotificationFromDashboard(eventId)
+            setTimeout(() => {
+                setEventRequestSuccess(false)
+                setEventRequestMessage('')
+            }, 2000)
+
+        } catch (err) {
+            console.log(err)
         }
+    }
+
+    const rejectEventHandler = async (eventId) => {
+        try {
+            await api.post(`/registration/${eventId}/rejections`, {}, { headers: { user } })
+            setEventRequestSuccess(true)
+            setEventRequestMessage('Event rejected successfully!')
+            removeNotificationFromDashboard(eventId)
+            setTimeout(() => {
+                setEventRequestSuccess(false)
+                setEventRequestMessage('')
+            }, 2000)
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const removeNotificationFromDashboard = (eventId) => {
+        const newEvents = eventsRequest.filter((event) => event._id !== eventId)
+        setEventsRequest(newEvents)
     }
 
     return (
         <>
-            <div className="left-panel">
-                <ListGroup>
-                    <Button id="testbutton" color="link" onClick={() => browseMapsFunc()}> Browse Maps </Button>
-                    <Button id="testbutton" color="link" onClick={() => myMapsFunc()}> My Maps </Button>
-                    <Button id="testbutton" color="link"> Shared with Me </Button>
-                    <Button id="testbutton" color="link"> Help </Button>
-                </ListGroup>
+            <ul className="notifications">
+                {eventsRequest.map(request => {
+                    return (
+                        <li key={request._id}>
+                            <div>
+                                <strong>{request.user.email} </strong> is requesting to register to your Event <strong>{request.event.title}</strong>
+                            </div>
+                            <ButtonGroup>
+                                <Button color="secondary" onClick={() => acceptEventHandler(request._id)}>Accept</Button>
+                                <Button color="danger" onClick={() => rejectEventHandler(request._id)}>Reject</Button>
+                            </ButtonGroup>
+                        </li>
+                    )
+                })}
+            </ul>
+            {eventRequestSuccess ? <Alert color="success"> {eventRequestMessage}</Alert> : ""}
+            <div className="filter-panel">
+                <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                    <DropdownToggle color="primary" caret>
+                        Filter
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem onClick={() => filterHandler(null)} active={rSelected === null} >All Sports</DropdownItem>
+                        <DropdownItem onClick={myEventsHandler} active={rSelected === 'myevents'} >My Events</DropdownItem>
+                        <DropdownItem onClick={() => filterHandler("running")} active={rSelected === 'running'} >Running</DropdownItem>
+                        <DropdownItem onClick={() => filterHandler("cycling")} active={rSelected === 'cycling'} >Cycling</DropdownItem>
+                        <DropdownItem color="primary" onClick={() => filterHandler('swimming')} active={rSelected === 'swimming'} >Swimming</DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
             </div>
-            {
-                browseMapsPanel ? (
-                    <div className="mapPanel">
-                        <h2>Browse Maps</h2>
-                        <ul>
-                            <li className="map">
-                                <strong> Map </strong>
-                                <img src={battle} width="200"/>
-                                <span> Date </span>
-                                <span> Description </span>
-                            </li>
+            <ul className="events-list">
+                {events.map(event => (
+                    < li key={event._id} >
+                        <header style={{ backgroundImage: `url(${event.thumbnail_url})` }}>
+                            {event.user === user_id ? <div><Button color="danger" size="sm" onClick={() => deleteEventHandler(event._id)}>Delete</Button></div> : ""}
 
-                            <li className="map">
-                                <strong> Map </strong>
-                                <img src={battle} width="200"/>
-                                <span> Date </span>
-                                <span> Description </span>
-                            </li>
-                        </ul>
-                    </div>
+                        </header>
+                        <strong>{event.title}</strong>
+                        <span>Event Date: {moment(event.date).format('l')}</span>
+                        <span>Event Price: {parseFloat(event.price).toFixed(2)}</span>
+                        <span>Event Description: {event.description}</span>
+                        <Button color="primary" onClick={() => registrationRequestHandler(event)}>Registration Request</Button>
+                    </li>
+                ))}
+            </ul>
+            {
+                error ? (
+                    <Alert className="event-validation" color="danger"> {messageHandler} </Alert>
                 ) : ""
             }
             {
-                myMapsPanel ? (
-                    <div className="mapPanel">
-                        <h2>My Maps</h2>
-                        <ul>
-                            <li className="map">
-                                <strong> Map </strong>
-                                <img src={battle} width="200"/>
-                                <span> Date </span>
-                                <span> Description </span>
-                            </li>
-
-                            <li className="map">
-                                <strong> Map </strong>
-                                <img src={battle} width="200"/>
-                                <span> Date </span>
-                                <span> Description </span>
-                            </li>
-                        </ul>
-                    </div>
+                success ? (
+                    <Alert className="event-validation" color="success"> {messageHandler}</Alert>
                 ) : ""
             }
-
-            
         </>
     )
 }
